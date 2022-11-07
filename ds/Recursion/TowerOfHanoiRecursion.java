2f283bb73d130123b6795fb6158816222ec867ce
sarvesh <sarveshbhogale4@gmail.com>
2022-11-07 21:33:48 +0530

    Tower of Hanoi

--- ds/Recursion/TowerOfHanoiRecursion.java
+++ ds/Recursion/TowerOfHanoiRecursion.java
@@ -4,19 +4,19 @@ public class TowerOfHanoiRecursion {
 	
 	 public static void main(String[] args) {
 		 int n=3;
-		 towerOfHanoi(n,'A','C','A');
+		 towerOfHanoi(n,'A','B','C');
 	 }
 
-	private static void towerOfHanoi(int n, char from_rod, char to_rod, char helper_rod) {
+	private static void towerOfHanoi(int n, char from_rod, char helper_rod, char to_rod) {
 	
 		if(n==1) {
 			System.out.println("Disk 1 moved from "+ from_rod + " to " + to_rod);
 			return;
 		}
 		
-		towerOfHanoi(n-1,from_rod,helper_rod,to_rod);
+		towerOfHanoi(n-1,from_rod,to_rod,helper_rod);
 		System.out.println("Disk " + n + " moved from "+ from_rod + " to " + to_rod);
-		towerOfHanoi(n-1,helper_rod,to_rod,from_rod);
+		towerOfHanoi(n-1,helper_rod,from_rod,to_rod);
 		
 	}
 
